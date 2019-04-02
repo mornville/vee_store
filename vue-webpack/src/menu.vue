66ec10b87a8f8caaa81005939426d4ef5517674c
Ashutosh Jha <aj97389@gmail.com>
2019-04-02 16:59:29 +0530

    added images

--- vue-webpack/src/menu.vue
+++ vue-webpack/src/menu.vue
@@ -31,7 +31,7 @@
                 <main class="mw6 center"   v-for="i in main">
                         <article class="dt w-100 bb b--black-05 pb2 mt2" href="#0">
                         <div class="dtc w2 w3-ns v-mid">
-                            <img src="http://mrmrs.github.io/photos/p/2.jpg" class="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns"/>
+                            <img src="src/assets/chick.jpg" class="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns"/>
                         </div>
                         <div class="dtc v-mid pl3">
                             <h1 class="f6 f5-ns fw6 lh-title black mv0">{{ i.name }} </h1>
@@ -53,7 +53,7 @@
                 <main class="mw6 center"   v-for="i in deserts">
                         <article class="dt w-100 bb b--black-05 pb2 mt2" href="#0">
                         <div class="dtc w2 w3-ns v-mid">
-                            <img src="http://mrmrs.github.io/photos/p/2.jpg" class="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns"/>
+                            <img src="src/assets/black.jpg" class="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns"/>
                         </div>
                         <div class="dtc v-mid pl3">
                             <h1 class="f6 f5-ns fw6 lh-title black mv0">{{ i.name }} </h1>
