9eaf8f825a37ea2eeb7b80b3d0e6a016e649a305
Ashutosh Jha <aj97389@gmail.com>
2019-04-03 22:43:52 +0530

    added bootstrap modal and changed cart image

--- vue-webpack/src/menu.vue
+++ vue-webpack/src/menu.vue
@@ -76,48 +76,55 @@
 
             </div>
 
-            <div class="interior"  id="mybutton">
-  
-                         <a href="#open-modal"  style="color:white;text-decoration:none;"><span class=" cart-notif">{{ cart.length }}</span><i class=" car fa fa-shopping-bag "></i></a>
+            <!-- Button trigger modal -->
+                <span style="color:white" id="mybutton" data-toggle="modal" data-target="#exampleModalLong"><span class="cart-notif" style="box-shadow: 0 15px 30px 0 rgba(0,0,0,0.11),
+                        0 5px 15px 0 rgba(0,0,0,0.08);">{{ cart.length }}</span><i class=" car fa fa-shopping-bag "></i>
+                </span>
 
-            </div>
-                   
-            <div id="open-modal" class="modal-window" style="text-align:center">
-                <div class="container">
-                     <a href="#" title="Close" class="modal-close"><span class="fa fa-times"></span></a>
-                        <h1>Cart({{ cart.length }})</h1>
-                <div>
-                <div v-if="!cart.length">
-                    
-                        <h1>
-                       Why Empty? Buy Somethinng :)
-                        </h1>
-                   
-                </div>    
-               <ul class="list pl0 mt0 measure center">
-                    <li    class="flex items-center lh-copy pa3 ph0-l bb b--black-10" v-for="item in cart"> 
-                        <img class="w2 h2 w3-ns h3-ns br-100" src="src/assets/tikka.jpg" />
-                        <div class="pl3 flex-auto">
-                            <span class="f6 db black-70"> {{ item.name }}</span>
-                            <span class="f6 db black-70">Rs. {{item.price}}</span>
-                        </div>
-                        <div>
-                            <a  class="f6 link blue hover-dark-gray"> <button v-on:click="addToCart(item)">+</button> 
-                                        {{ item.quantity }} 
-                                        <button v-on:click="removeFromCart(item)">-</button> </a>
-                        </div>
-                    </li>     
+                <!-- Modal -->
+                <div class="modal fade" style="text-align:center" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle"  style="text-align:center !important">Cart({{ cart.length }})</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div v-if="!cart.length">
+                            <h1>
+                                Why Empty? Buy Somethinng :)
+                            </h1>                   
+                        </div>    
+                        <ul class="list pl0 mt0 measure center">
+                                <li    class="flex items-center lh-copy pa3 ph0-l bb b--black-10" v-for="item in cart"> 
+                                    <img class="w2 h2 w3-ns h3-ns br-100" src="src/assets/tikka.jpg" />
+                                    <div class="pl3 flex-auto">
+                                        <span class="f6 db black-70"> {{ item.name }}</span>
+                                        <span class="f6 db black-70">Rs. {{item.price}}</span>
+                                    </div>
+                                    <div>
+                                        <a  class="f6 link blue hover-dark-gray"> <button v-on:click="addToCart(item)">+</button> 
+                                                    {{ item.quantity }} 
+                                                    <button v-on:click="removeFromCart(item)">-</button> </a>
+                                    </div>
+                                </li>     
 
-    <br><br><br>
-               <button v-on:click="checkout(item.quantity,item.price)">CheckOut ( {{ sum }}/- )  </button> 
-               </ul>
-               
-    
-
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">BUY MORE?</button>
+                        <button type="button" class="btn btn-primary">CHECKOUT ( {{ sum }}/- )</button>
+                    </div>
+                    </div>
                 </div>
-                       
                 </div>
-            </div>
+
+
+
+
+            
             <div id="snackbar">Added To Cart..</div>
         </div>
 
@@ -128,199 +135,204 @@
 
 
 
-export default {
-    
-  mounted() {
-    if(localStorage.getItem('cart')) {
-        try {
-            this.cart = JSON.parse(localStorage.getItem('cart'));
-            
-        }
-        catch(e) {
-            localStorage.removeItem('cart');
-        }
-    }
-  },
-   data() {
-    return{
-            total:0,
-            items: [
-                { 
-                name: 'Paneer Manchurian', 
-                itemID: 'jk778aA', 
-                price: 250, 
-                description: "",
-                quantity: 0
-                },
-                { 
-                    name: 'Coca Cola', 
-                    itemID: 'sadjlk823', 
-                    price: 100,
-                    description: "",
-                    quantity: 0
-                },
-                { 
-                    name: 'Bisleri 1L', 
-                    itemID: 'kja767', 
-                    price: 30,
-                    description: "",
-                    quantity: 0
-                },
-                   { 
-                    name: 'Chai', 
-                    itemID: 'k2ja767', 
-                    price: 30,
-                    description: "",
-                    quantity: 0
-                },
-                 { 
-                    name: 'Coffee', 
-                    itemID: '33jhs', 
-                    price: 30,
-                    description: "",
-                    quantity: 0
-                },
-                  { 
-                    name: 'LOL?', 
-                    itemID: '334j3hs', 
-                    price: 30000000,
-                    description: "",
-                    quantity: 0
-                },
-            ],
-            main: [
-            { 
-                name: 'Paneer Butter Masala', 
-                itemID: 'jk778a', 
-                price: 250, 
-                description: "",
-                quantity: 0
-                },
-                { 
-                    name: 'Butter Chicken', 
-                    itemID: 'sadj823', 
-                    price: 100,
-                    description: "",
-                    quantity: 0
-                },
-                { 
-                    name: 'tandoori Chicken', 
-                    itemID: 'kj767', 
-                    price: 30,
-                    description: "",
-                    quantity: 0
-                },
-                   { 
-                    name: 'Chana Masla', 
-                    itemID: 'k2ja67', 
-                    price: 30,
-                    description: "",
-                    quantity: 0
-                },
-            ],
-            deserts:[
-                {name: 'Mango', cost:70},
-                {name: 'Strawberry', cost:70},
-                {name: 'ButterScotch', cost:70},
-                {name: 'Black Currant', cost:70},
-                {name: 'Vanilla', cost:70}
-            ],
-            cart:[],
-            sum:0,
-
-    
-        }
-    
-  },
- methods: {
-      addToCart: function(item) {
-          console.log(this.sum);
-         
-          var x = document.getElementById("snackbar");
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
-
-          if (this.cart.length == 0) {
-              item.quantity += 1;
-              this.sum = this.sum + item.price;
-              this.cart.push(item);
-          }
-          else {
-              var found = false;
-            this.cart.forEach(element => {
-                if (element.itemID === item.itemID) {
-                    element.quantity += 1;
-                     this.sum = this.sum + item.price;
-                    found = true;
-                }
-            });
-            if (found==false) {
-                item.quantity += 1
-                 this.sum = this.sum + item.price;
-                this.cart.push(item);
-            }
-          }
-          // Saving item to cart
-          const parsed = JSON.stringify(this.cart);
-          localStorage.setItem('cart', parsed);
-      },
-      quantity: function(item) {
-        var quant = 0;
-        this.cart.forEach(element => {
-            if(element.itemID == item.itemID) {
-                quant = element.quantity
-            }
-        });
-        return quant;
-      },
-      reverse(){
-          this.total++;
-      },
-      removeFromCart: function(item) {
-              console.log("executing this!!");
-              if (this.cart.length == 0) {
-              return;
-              }
-              else {
-                  var found = false;
-                this.cart.forEach(element => {
-                    if (element.itemID === item.itemID) {
+            export default {
+                
+            mounted() {
+                if(localStorage.getItem('cart')) {
+                    try {
+                        this.cart = JSON.parse(localStorage.getItem('cart'));
                         
-                        element.quantity -= 1;
-                         this.sum = this.sum - item.price;
-                        found = true;
                     }
-                });
-                if (found==false) {
-                    item.quantity -= 1
-                     this.sum = this.sum - item.price;
-                    this.cart.push(item);
+                    catch(e) {
+                        localStorage.removeItem('cart');
+                    }
                 }
-              }
-              // Saving item to cart
-              this.removeZero();
-              const parsed = JSON.stringify(this.cart);
-              localStorage.setItem('cart', parsed);
-          },
-          removeZero(){
-              var newCart = [];
-              this.cart.forEach(element => {
-                  if(element.quantity != 0) {
-                      newCart.push(element);
-                  } 
-              });
-              this.cart = newCart;
-          },
-  },
-}
+                
+
+                
+            },
+            data() {
+                return{
+                        total:0,
+                        items: [
+                            { 
+                            name: 'Paneer Manchurian', 
+                            itemID: 'jk778aA', 
+                            price: 250, 
+                            description: "",
+                            quantity: 0
+                            },
+                            { 
+                                name: 'Coca Cola', 
+                                itemID: 'sadjlk823', 
+                                price: 100,
+                                description: "",
+                                quantity: 0
+                            },
+                            { 
+                                name: 'Bisleri 1L', 
+                                itemID: 'kja767', 
+                                price: 30,
+                                description: "",
+                                quantity: 0
+                            },
+                            { 
+                                name: 'Chai', 
+                                itemID: 'k2ja767', 
+                                price: 30,
+                                description: "",
+                                quantity: 0
+                            },
+                            { 
+                                name: 'Coffee', 
+                                itemID: '33jhs', 
+                                price: 30,
+                                description: "",
+                                quantity: 0
+                            },
+                            { 
+                                name: 'LOL?', 
+                                itemID: '334j3hs', 
+                                price: 30000000,
+                                description: "",
+                                quantity: 0
+                            },
+                        ],
+                        main: [
+                        { 
+                            name: 'Paneer Butter Masala', 
+                            itemID: 'jk778a', 
+                            price: 250, 
+                            description: "",
+                            quantity: 0
+                            },
+                            { 
+                                name: 'Butter Chicken', 
+                                itemID: 'sadj823', 
+                                price: 100,
+                                description: "",
+                                quantity: 0
+                            },
+                            { 
+                                name: 'tandoori Chicken', 
+                                itemID: 'kj767', 
+                                price: 30,
+                                description: "",
+                                quantity: 0
+                            },
+                            { 
+                                name: 'Chana Masla', 
+                                itemID: 'k2ja67', 
+                                price: 30,
+                                description: "",
+                                quantity: 0
+                            },
+                        ],
+                        deserts:[
+                            {name: 'Mango', cost:70},
+                            {name: 'Strawberry', cost:70},
+                            {name: 'ButterScotch', cost:70},
+                            {name: 'Black Currant', cost:70},
+                            {name: 'Vanilla', cost:70}
+                        ],
+                        cart:[],
+                        sum:0,
+
+                
+                    }
+                
+            },
+            methods: {
+                addToCart: function(item) {
+                    console.log(this.sum);
+                    
+                    var x = document.getElementById("snackbar");
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
+
+                    if (this.cart.length == 0) {
+                        item.quantity += 1;
+                        this.sum = this.sum + item.price;
+                        this.cart.push(item);
+                    }
+                    else {
+                        var found = false;
+                        this.cart.forEach(element => {
+                            if (element.itemID === item.itemID) {
+                                element.quantity += 1;
+                                this.sum = this.sum + item.price;
+                                found = true;
+                            }
+                        });
+                        if (found==false) {
+                            item.quantity += 1
+                            this.sum = this.sum + item.price;
+                            this.cart.push(item);
+                        }
+                    }
+                    // Saving item to cart
+                    const parsed = JSON.stringify(this.cart);
+                    localStorage.setItem('cart', parsed);
+                },
+                quantity: function(item) {
+                    var quant = 0;
+                    this.cart.forEach(element => {
+                        if(element.itemID == item.itemID) {
+                            quant = element.quantity
+                        }
+                    });
+                    return quant;
+                },
+                reverse(){
+                    this.total++;
+                },
+                removeFromCart: function(item) {
+                        console.log("executing this!!");
+                        if (this.cart.length == 0) {
+                        return;
+                        }
+                        else {
+                            var found = false;
+                            this.cart.forEach(element => {
+                                if (element.itemID === item.itemID) {
+                                    
+                                    element.quantity -= 1;
+                                    this.sum = this.sum - item.price;
+                                    found = true;
+                                }
+                            });
+                            if (found==false) {
+                                item.quantity -= 1
+                                this.sum = this.sum - item.price;
+                                this.cart.push(item);
+                            }
+                        }
+                        // Saving item to cart
+                        this.removeZero();
+                        const parsed = JSON.stringify(this.cart);
+                        localStorage.setItem('cart', parsed);
+                    },
+                    removeZero(){
+                        var newCart = [];
+                        this.cart.forEach(element => {
+                            if(element.quantity != 0) {
+                                newCart.push(element);
+                            } 
+                        });
+                        this.cart = newCart;
+                    },
+            },
+            }
 </script>
+
+
 <style>
 .feedback {
 
   padding: 10px 20px;
   
 }
-a > .cart-notif {
+.cart-notif {
   display: inline-block;
   position: relative;
   padding-left: 0px;
@@ -340,75 +352,20 @@ a > .cart-notif {
   position: fixed;
   bottom:2px;
   right: 10px;
+  
 }
-.modal-window {
-	 position: fixed;
-	 background-color: rgba(255, 255, 255, 0.25);
-	 top: 0;
-	 right: 0;
-	 bottom: 0;
-	 left: 0;
-	 z-index: 999;
-	 opacity: 0;
-	 pointer-events: none;
-	 -webkit-transition: all 0.3s;
-	 -moz-transition: all 0.3s;
-	 transition: all 0.3s;
-}
+
+
 .car{
-    background:black;
+    background:rgb(124, 124, 72);
     padding:20px;
     font-size:40px;
     border-radius: 50%;
-}
- .modal-window:target {
-	 opacity: 1;
-	 pointer-events: auto;
-}
- .modal-window > div {
-	 width: 80%;
-	 position: absolute;
-	 top: 50%;
-	 left: 50%;
-	 transform: translate(-50%, -50%);
-	 padding: 2em;
-	 background: #fff;
-	 color: #333;
-}
- .modal-window header {
-	 font-weight: bold;
-}
- .modal-window h1 {
-	 font-size: 150%;
-	 margin: 0 0 15px;
-	 color: #333;
-}
- .modal-close {
-	 color: #aaa;
-	 line-height: 50px;
-	 font-size: 80%;
-	 position: absolute;
-	 right: 0;
-	 text-align: center;
-	 top: 0;
-	 width: 70px;
-	 text-decoration: none;
-}
- .modal-close:hover {
-	 color: #000;
+    
 }
 
 
- .btn {
-	 background-color: #fff;
-	 padding: 1em 1.5em;
-	 border-radius: 3px;
-	 text-decoration: none;
-}
- .btn i {
-	 padding-right: 0.3em;
- }
- 
+
  #snackbar {
   visibility: hidden;
   min-width: 250px;
