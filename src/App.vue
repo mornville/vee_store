7e1e8589b422e210b27ffe75a7f9df82b277986d
Ashutosh Jha <aj97389@gmail.com>
2019-04-06 13:21:03 +0530

    indent app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,20 @@ import Message from './Message.vue';
 
 <template>
   <div id="app">
-
-     <Header store_name="Sadda Adda" store_desc="Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life" style="padding:20px"></Header>
-  
-
+    <!-- Header store name with background -->
+    <Header store_name="Sadda Adda" store_desc="Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life" style="padding:20px"></Header>
+    
+    
+    <!-- Rest Menu with cart button -->
     <div class="container">   
-
-  
-  
         <Menu></Menu>
         <br><br><br><br>
     </div>
+
+    <!-- Footer -->    
     <foot></foot>
+
+    <!-- End Vue App -->
   </div>
   
  
